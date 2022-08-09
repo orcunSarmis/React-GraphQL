78c26f8df79ff3e60ebfaf6ddac88ed302bb730d
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-09 21:29:57 +0200

    adding queries

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import github from "./db.js";
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
+import query from "./Query";
 
 function App() {
+const [userName, setUserName] = useState("");
+const [repoList, setRepoList] = useState(null);
+
+const fetchData = useCallback(() => {
+  fetch(github.baseURL, {
+    method: "POST",
+    headers: github.headers,
+    body: JSON.stringify(query),
+  })
+  .then(response => response.json())
+  .then(data => {
+    const viewer = data.data.viewer;
+    setUserName(viewer.name);
+    setRepoList(viewer.repositories.nodes);
+    setUserName(data.data.viewer.name)
+    console.log(data);      
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}, []);
 
   useEffect(() => {
-    const githubQuery = {
-      query: `
-        {
-          viewer {
-            name
-          }
-        }
-      `,
-    };
-
-    fetch(github.baseURL, {
-      method: "POST",
-      headers: github.headers,
-      body: JSON.stringify(githubQuery),
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  });
+    fetchData();
+  }, [fetchData]);
   
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i>Repos</h1>
+        <p>Hey there {userName}</p>
+
+        {
+          repoList && (
+            <ul className="list-group list-group-flush">
+              {
+                repoList.map((repo) => (
+                  <li className="list-group-item" key={repo.id.toString()}>
+                    <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                      {repo.name}
+                    </a>
+                    <p className="small">{repo.description}</p>
+                  </li>
+                ))}
+            </ul>
+          )
+        }
     </div>
   );
 }
