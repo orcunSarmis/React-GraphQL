2db2fd148045dac1fa1c38cb8a49909ba0891399
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-09 19:42:21 +0200

    working on graphql application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import github from "./db.js";
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+        {
+          viewer {
+            name
+          }
+        }
+      `,
+    };
+
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+  
   return (
     <div className="App container mt-5">
-      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i>Repos</h1>
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>Repos</h1>
     </div>
   );
 }
